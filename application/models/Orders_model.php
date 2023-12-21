fd517d90a0b8ce1b9285f3493572b0430045da90
Beutech AP03 <ap03@beutech.nl>
2023-12-21 15:49:47 +0000

    Duitsland

--- application/models/Orders_model.php
+++ application/models/Orders_model.php
@@ -730,8 +730,8 @@ class Orders_model extends CI_Model { // Orders Model class
 				$order_last_modified_king = system_to_euro_date_time($order_last_modified_king);
 				$orderregel_last_modified_king = system_to_euro_date_time($orderregel_last_modified_king);
 				
-				if($add_to_db == 0){ //IF orderregel exists -> UPDATE ALL
-				//if(($add_to_db == 0) && (($order_last_modified_king != $lastupdate_king) || ($orderregel_last_modified_king != $lastupdate_regel_king))){ //IF orderregel exists but last update is not equal then update row
+				//if($add_to_db == 0){ //IF orderregel exists -> UPDATE ALL
+				if(($add_to_db == 0) && (($order_last_modified_king != $lastupdate_king) || ($orderregel_last_modified_king != $lastupdate_regel_king))){ //IF orderregel exists but last update is not equal then update row
 				
 					$update_db = 1;
 					
